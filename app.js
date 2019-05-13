eea1065a1ea37419c60452ccb6bff7d9c10f0c71
vincent <564779653@qq.com>
2019-05-13 16:54:02 +0800

    update

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cors({
   allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
 }))
 
-// 设置无序token验证的API
+// 设置无需token验证的API
 app.use(koaJwt({secret: secret.sign}).unless({
   path: [
     /^\/v1\/auth/,
